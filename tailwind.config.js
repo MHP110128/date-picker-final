b19720017dd9b29a82af75bbd0510ff1328d4b1d
MHPourhasani <mhpourhasani.dev@gmail.com>
2023-04-17 17:13:40 +0430

    added fonts and custom styles in tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        iranyekan: ["iranyekan"],
+      },
+      fontSize: {
+        11: "11px",
+        14: "14px",
+        15: "15px",
+        16: "16px",
+      },
+      colors: {
+        white: "#fff",
+        primary: "#3880FF",
+        secondary300: "#D9DBE9",
+        secondary400: "#A0A3BD",
+        secondary600: "#4E4B66",
+        secondary800: "#14142A",
+      },
+      borderWidth: { 1: "1px", 1.5: "1.5px" },
+      boxShadow: {
+        calendar: "-4px 4px 16px rgba(0, 0, 0, 0.08)",
       },
     },
   },
   plugins: [],
-}
+};
